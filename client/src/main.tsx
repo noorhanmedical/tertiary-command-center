c0a773a9396216ba5c55f1b9fdd4bff5a478ff35
noorhanmedical <drimran@noorhanmedical.com>
2026-06-12 16:30:40 -0700

    Remove Plexus IQ CSS import

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./premiumRefinement.css";
-import "./plexusIqPremium.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
